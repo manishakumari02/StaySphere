e8caf0fb035b03d65cc19eea9232f319174a8c0a
Manisha Kumari Sah <kumarimanisha2682@gmail.com>
2025-06-18 14:08:50 +0530

    server side validation

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync=require("./utils/wrapAsync.js");
 const ExpressError=require("./utils/ExpressError.js");
+const {listingSchema}=require("./schema.js");
 
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
@@ -31,6 +32,17 @@ app.get("/", (req, res) => {
     res.send("Hi i am root path");
 })
 
+const validateListing=(req,res,next)=>{
+      let {error}=listingSchema.validate(req.body);
+        if(error){
+            let errorMsg=error.details.map((el)=>el.message).join(",");
+            throw new ExpressError(404,errorMsg);
+
+        }
+        else{
+            next();
+        }
+}
 
 //index route
 app.get("/listings", wrapAsync(async (req, res) => {
@@ -45,11 +57,8 @@ app.get("/listings/new", (req, res) => {
 })
 
 //create route
-app.post("/listings", wrapAsync(async(req, res, next) => {
+app.post("/listings",validateListing, wrapAsync(async(req, res, next) => {
     // let {title,description,image,price,country,location}=req.body;
-    if(!req.body.listings){
-        throw new ExpressError(404,"send valid data for listing");
-    }
         const newListing = new Listing(req.body.listing);
         await newListing.save();
         res.redirect("/listings");
@@ -69,10 +78,10 @@ app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
     res.render("listings/edit", { listing });
 }))
 //UPDATE ROUTE
-app.put("/listings/:id", wrapAsync(async (req, res) => {
-    if(!req.body.listings){
-        throw new ExpressError(404,"send valid data for listing");
-    }
+app.put("/listings/:id",validateListing, wrapAsync(async (req, res) => {
+    // if(!req.body.listings){
+    //     throw new ExpressError(404,"send valid data for listing");
+    // }
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, { ...req.body.listing });
     res.redirect(`/listings/${id}`);
@@ -108,8 +117,9 @@ app.delete("/listings/:id", wrapAsync(async (req, res) => {
 //err handeling route
 app.use((err, req, res, next) => {
     let {statusCode=500,message="something went wrong"}=err;
-    res.status(statusCode).send(message);
+    // res.status(statusCode).send(message);
     // res.send("something went wrong");
+    res.status(statusCode).render("error.ejs",{message});
 });
 
 app.listen(8080, () => {
