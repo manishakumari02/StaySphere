1f2f07f2e50e264debdbd743d1f8ff9d346c5ab4
Manisha Kumari Sah <kumarimanisha2682@gmail.com>
2025-06-20 17:35:31 +0530

    Add review model, review form, and POST route to save reviews

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const ejsMate = require("ejs-mate");
 const wrapAsync=require("./utils/wrapAsync.js");
 const ExpressError=require("./utils/ExpressError.js");
 const {listingSchema}=require("./schema.js");
+const Review=require("./models/review.js")
+
 
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
@@ -94,6 +96,21 @@ app.delete("/listings/:id", wrapAsync(async (req, res) => {
     res.redirect("/listings");
 }))
 
+//Review route
+//post rooute
+app.post("/listings/:id/reviews",async(req,res)=>{
+    let listing=await Listing.findById(req.params.id);
+    let newReview=new Review(req.body.review);
+
+    listing.reviews.push(newReview);
+
+    await newReview.save();
+    await listing.save();
+
+    // console.log("new review save");
+    // res.send("new review saved");
+    res.redirect(`/listings/${listing._id}`);
+})
 // app.get("/testListing",async(req,res)=>{
 //     let sampleListing=new Listing({
 //         title:"My new Villa",
