d4663dc6c9f53cf87a811d1c145e6d80be6e5d50
Manisha Kumari Sah <kumarimanisha2682@gmail.com>
2025-07-18 22:57:30 +0530

    creating post login page

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router=express.Router();
 const User=require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync");
 const passport = require("passport");
+const {saveRedirectUrl}=require("../middleware.js");
 
 router.get("/signup",(req,res)=>{
     res.render("./users/signup.ejs");
@@ -31,10 +32,11 @@ router.post("/signup",wrapAsync(async(req,res)=>{
 router.get("/login",(req,res)=>{
     res.render("./users/login.ejs");
 })
-router.post("/login",passport.authenticate("local",{failureRedirect:"/login",failureFlash:true}),
+router.post("/login",saveRedirectUrl,passport.authenticate("local",{failureRedirect:"/login",failureFlash:true}),
 async(req,res)=>{
     req.flash("success","welcome to wanderlust! you are logged in");
-    res.redirect("/listings");
+    let redirectUrl=res.locals.redirect || "/listings";
+    res.redirect(redirectUrl);
 })
 
 router.get("/logout",(req,res,next)=>{
