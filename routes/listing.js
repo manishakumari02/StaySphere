328ad525e77ff65572a66d40e62a71090bf9363b
Manisha Kumari Sah <kumarimanisha2682@gmail.com>
2025-07-19 17:31:13 +0530

    authorization for listing

--- routes/listing.js
+++ routes/listing.js
@@ -1,23 +1,10 @@
 const express=require("express");
 const router=express.Router();
 const wrapAsync=require("../utils/wrapAsync.js");
-const ExpressError=require("../utils/ExpressError.js");
-const {listingSchema}=require("../schema.js");
 const Listing = require("../models/listing.js");
-const {isLoggedIn}=require("../middleware.js");
+const {isLoggedIn, isOwner,validateListing}=require("../middleware.js");
 
 
-const validateListing=(req,res,next)=>{
-    let {error}=listingSchema.validate(req.body);
-        if(error){
-            let errorMsg=error.details.map((el)=>el.message).join(",");
-            throw new ExpressError(400,errorMsg);
-
-        }
-        else{
-            next();
-        }
-}
 
 //index route
 router.get("/", wrapAsync(async (req, res) => {
@@ -54,7 +41,7 @@ router.get("/:id", wrapAsync(async (req, res) => {
     res.render("listings/show", { listing });
 }))
 //Edit route
-router.get("/:id/edit",isLoggedIn, wrapAsync(async (req, res) => {
+router.get("/:id/edit",isLoggedIn,isOwner, wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
     if(!listing){
@@ -64,7 +51,7 @@ router.get("/:id/edit",isLoggedIn, wrapAsync(async (req, res) => {
     res.render("listings/edit", { listing });
 }))
 //UPDATE ROUTE
-router.put("/:id",isLoggedIn,validateListing, wrapAsync(async (req, res) => {
+router.put("/:id",isLoggedIn,isOwner,validateListing, wrapAsync(async (req, res) => {
     // if(!req.body.listings){
     //     throw new ExpressError(404,"send valid data for listing");
     // }
@@ -74,7 +61,7 @@ router.put("/:id",isLoggedIn,validateListing, wrapAsync(async (req, res) => {
     res.redirect(`/listings/${id}`);
 }))
 //DELETE ROUTE
-router.delete("/:id",isLoggedIn, wrapAsync(async (req, res) => {
+router.delete("/:id",isLoggedIn,isOwner, wrapAsync(async (req, res) => {
     let { id } = req.params;
     let deletedRes = await Listing.findByIdAndDelete(id);
     // console.log(deletedRes);
--- routes/review.js
+++ routes/review.js
@@ -2,23 +2,11 @@ const express=require("express");
 const router=express.Router({mergeParams:true});  //child route used in parents like id so reviews find by id and work
 const wrapAsync=require("../utils/wrapAsync.js");
 const ExpressError=require("../utils/ExpressError.js");
-const {reviewSchema}=require("../schema.js");
 const Review=require("../models/review.js");
 const Listing = require("../models/listing.js");
+const {validateReview}=require("../middleware.js");
 
 
-
-const validateReview=(req,res,next)=>{
-      let {error}=reviewSchema.validate(req.body);
-        if(error){
-            let errorMsg=error.details.map((el)=>el.message).join(",");
-            throw new ExpressError(404,errorMsg);
-
-        }
-        else{
-            next();
-        }
-}
 
 //Review route
 //post review rooute
