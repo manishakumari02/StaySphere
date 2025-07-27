57f807b6b531896f29f8f58e4510bddc63067628
Manisha Kumari Sah <kumarimanisha2682@gmail.com>
2025-07-27 15:33:58 +0530

    Image preview for edit page

--- controllers/listings.js
+++ controllers/listings.js
@@ -24,7 +24,9 @@ module.exports.renderEditForm=async (req, res) => {
         req.flash("error","listing you requested for does not exist!");
         return res.redirect("/listings");
     }
-    res.render("listings/edit", { listing });
+    let originalImageUrl=listing.image.url;
+    originalImageUrl=originalImageUrl.replace("/upload","/upload/w_250")
+    res.render("listings/edit", { listing,originalImageUrl });
 }
 module.exports.updateListing=async (req, res) => {
     // if(!req.body.listings){
