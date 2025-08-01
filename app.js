30f2acd61691ffa553f7fc08479ff9151a7ced26
Manisha Kumari Sah <kumarimanisha2682@gmail.com>
2025-08-01 14:46:00 +0530

    update package.js for deploy

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use(express.static(path.join(__dirname, "/public")));
 const store=MongoStore.create({
     mongoUrl:dbURL,
     crypto:{
-        secret:"mysupersecretcode",
+        secret:process.env.SECRET,
     },
     touchAfter:24*3600,
 })
@@ -58,7 +58,7 @@ store.on("error",()=>{
 })
 const sessionOptions={
     store,
-    secret:"mysupersecretcode",
+    secret:process.env.SECRET,
     resave:false,
     saveUninitialized:true,
     cookie:{
